886de64ea60f0f895d6101027b1bfbb55ca50e7b
sabih khan <sabihop56@gmail.com>
2026-06-08 21:16:04 +0500

    added the create read files

--- kernel.c
+++ kernel.c
@@ -1,31 +1,27 @@
 #include <stdint.h>
 
 // vga starting point
+
 int startstringpoint = 0;
+
 // user data which is typed in shell
+
 char inputdata[50];
-
-#define MAX_FILES 256
-#define directory_sectors 10
-#define start_directory_lba 5
-
-struct File_entry
+char filedata[500];
+struct FileEntry
 {
     char name[12];
     uint32_t start_lba;
     uint32_t is_used;
 };
 
-struct File_entry DIRECTORY_TABLES[MAX_FILES];
+#define MAX_FILES 256
+#define DIRECTORY_SECTORS 10
+#define START_DIR_LBA 5
 
-// // before cotinue read this again it will hlp you understand
-// This is the address of the 41st slot (Index 40).When you write arr + 40,
-// the computer calculates: Start + (40 * size of element).If you want to put 324 at index 40,
-// you would have to write: *(arr + 40) = 324;
+struct FileEntry directory_table[MAX_FILES];
 
-
-
-
+// Load all 10 sectors into memory
 
 unsigned char keyboard_map[128] = {
     0, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b', /* Backspace */
@@ -160,6 +156,195 @@ char getKeyPress()
     return keyboard_map[scancode];
 }
 
+void outb(unsigned short port, unsigned char data)
+{
+    __asm__ volatile("outb %0,%1" : : "a"(data), "Nd"(port));
+}
+
+void outw(unsigned short port, unsigned short data)
+{
+    __asm__ volatile("outw %0, %1" : : "a"(data), "Nd"(port));
+}
+
+void read_disk(uint32_t lba, uint8_t *buffer)
+{
+    // Wait until the drive is not Busy (BSY bit 7 must be 0) before sending ports
+    while (inputbyte(0x1F7) & 0x80)
+        ;
+
+    outb(0x1F2, 1);                    // Sector count
+    outb(0x1F3, (uint8_t)lba);         // LBA Low
+    outb(0x1F4, (uint8_t)(lba >> 8));  // LBA Mid
+    outb(0x1F5, (uint8_t)(lba >> 16)); // LBA High
+                                       // outb(0x1F6, 0xE0 | ((lba >> 24) & 0x0F)); // Drive select
+    outb(0x1F6, 0xA0);
+    outb(0x1F7, 0x20); // Send READ command
+
+    uint32_t timeout2 = 0;
+    while ((inputbyte(0x1F7) & 0x08) == 0)
+    {
+        timeout2++;
+        if (timeout2 >= 500000)
+        {
+            print("can't read file\n", 0x0F, (char *)0xB8000, 0);
+            return;
+        }
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        uint16_t data = 0;
+        __asm__ volatile("inw %%dx, %%ax" : "=a"(data) : "d"(0x1F0));
+        buffer[i * 2] = (uint8_t)data;
+        buffer[i * 2 + 1] = (uint8_t)(data >> 8);
+    }
+}
+
+void write_disk_sector(uint32_t lba, uint8_t *buffer)
+{
+    // Wait until the drive is not Busy (BSY bit 7 must be 0) before sending ports
+    while (inputbyte(0x1F7) & 0x80)
+        ;
+
+    outb(0x1F2, 1);
+    outb(0x1F3, (uint8_t)lba);
+    outb(0x1F4, (uint8_t)(lba >> 8));
+    outb(0x1F5, (uint8_t)(lba >> 16));
+    // outb(0x1F6, 0xE0 | ((lba >> 24) & 0x0F));
+    outb(0x1F6, 0xA0);
+    outb(0x1F7, 0x30); // Send WRITE command
+
+    uint32_t timeout = 0;
+    while ((inputbyte(0x1F7) & 0x08) == 0)
+    {
+        timeout++;
+        if (timeout >= 500000)
+        {
+            print("can't make file\n", 0x0F, (char *)0xB8000, 0);
+            return;
+        }
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        uint16_t data = buffer[i * 2] | (buffer[i * 2 + 1] << 8);
+        __asm__ volatile("outw %%ax, %%dx" : : "a"(data), "d"(0x1F0));
+    }
+
+    // Explicitly wait for the drive to flush its cache write changes
+    while (inputbyte(0x1F7) & 0x80)
+        ;
+}
+
+void load_directory()
+{
+    uint8_t sector_buffer[512];
+    uint8_t *table_ptr = (uint8_t *)directory_table;
+
+    for (int i = 0; i < DIRECTORY_SECTORS; i++)
+    {
+        read_disk(START_DIR_LBA + i, sector_buffer);
+
+        // Safely copy sector by sector into RAM storage
+        for (int j = 0; j < 512; j++)
+        {
+            table_ptr[(i * 512) + j] = sector_buffer[j];
+        }
+    }
+}
+
+void save_directory()
+{
+    uint8_t sector_buffer[512];
+    uint8_t *table_ptr = (uint8_t *)directory_table;
+
+    for (int i = 0; i < DIRECTORY_SECTORS; i++)
+    {
+        // Build isolated temporary frame buffer
+        for (int j = 0; j < 512; j++)
+        {
+            sector_buffer[j] = table_ptr[(i * 512) + j];
+        }
+
+        write_disk_sector(START_DIR_LBA + i, sector_buffer);
+    }
+}
+
+void format_disk_directory()
+{
+    // 1. Wipe out our array in RAM completely
+    for (int i = 0; i < MAX_FILES; i++)
+    {
+        directory_table[i].is_used = 0;
+        directory_table[i].start_lba = 0;
+        for (int j = 0; j < 12; j++)
+        {
+            directory_table[i].name[j] = '\0';
+        }
+    }
+    save_directory();
+    // 2. Blast these clean zeros into disk sectors 5 through 14
+}
+
+void creatediskfile(char *name, char *content)
+{
+
+    load_directory();
+
+    for (int i = 0; i < MAX_FILES; i++)
+    {
+        if (directory_table[i].is_used == 0)
+        {
+
+            directory_table[i].start_lba = 15 + i;
+            directory_table[i].is_used = 1;
+
+            int j = 0;
+            while (name[j] != '\0' && j < 11)
+            {
+                directory_table[i].name[j] = name[j];
+                j++;
+            }
+            directory_table[i].name[j] = '\0';
+
+            uint8_t buffer[512] = {0};
+            j = 0;
+            while (content[j] != '\0' && j < 511)
+            {
+                buffer[j] = content[j];
+                j++;
+            }
+
+            write_disk_sector(directory_table[i].start_lba, buffer);
+
+            save_directory();
+            return;
+        }
+    }
+}
+
+void readfiles(char *name, char *video_memory)
+{
+
+    load_directory();
+    for (int i = 0; i < MAX_FILES; i++)
+    {
+
+        if (directory_table[i].is_used == 1 && (strcompare(directory_table[i].name, name) == 0))
+        {
+
+            uint8_t buffer[513] = {0};
+            buffer[512] = '\0';
+            read_disk(directory_table[i].start_lba, buffer);
+            print("starting to manage file ...\n", 0x0F, video_memory, 0);
+            print((char *)buffer, 0x0F, video_memory, 0);
+            print("\n", 0x0F, video_memory, 0);
+            return;
+        }
+    }
+    print("File not found on mega table!\n", 0x0F, video_memory, 0);
+}
+
 void initInputData(char *video_memory)
 {
     int i = 0;
@@ -228,6 +413,93 @@ void initInputData(char *video_memory)
                         char clear_line[2] = {'\n', '\0'};
                         print(clear_line, 0x0F, video_memory, 0);
                     }
+
+                    else if (inputdata[0] == 'm' && inputdata[1] == 'a' && inputdata[2] == 'k' &&
+                             inputdata[3] == 'e' && inputdata[4] == 'f' && inputdata[5] == 'i' &&
+                             inputdata[6] == 'l' && inputdata[7] == 'e')
+                    {
+
+                        if (inputdata[8] == '\0')
+                        {
+                            print("Error: Please provide a filename! Example: makefile hi.txt\n", 0x0F, video_memory, 0);
+                        }
+                        else
+                        {
+                            char mybuffer[500];
+                            char *filename = &inputdata[9];
+
+                            print("write what you want max 200 character\n", 0x0F, video_memory, 0);
+                            int idx = 0;
+                            while (1)
+                            {
+                                char a = getKeyPress();
+
+                                if (a != 0)
+                                {
+                                    // 1. Handle Backspace inside file writer
+                                    if (a == '\b')
+                                    {
+                                        if (idx > 0)
+                                        {
+                                            idx--;
+                                            mybuffer[idx] = '\0';
+                                            char back_str[2] = {'\b', '\0'};
+                                            print(back_str, 0x0F, video_memory, 0);
+                                        }
+                                        continue;
+                                    }
+
+                                    // 2. Handle Enter (Stop typing, save, and exit)
+                                    if (a == '\n')
+                                    {
+                                        mybuffer[idx] = '\0'; // Properly null-terminate the file string!
+                                        char newline_str[2] = {'\n', '\0'};
+                                        print(newline_str, 0x0F, video_memory, 0);
+
+                                        // Save to disk and print confirmation back to the screen
+                                        creatediskfile(filename, mybuffer);
+                                        // readfiles(filename, video_memory);
+                                        break; // Crucial: break out of the loop to return to main shell!
+                                    }
+
+                                    // 3. Handle Regular Characters
+                                    if (idx < 499)
+                                    {
+                                        mybuffer[idx] = a;
+                                        char echo_str[2] = {a, '\0'};
+                                        print(echo_str, 0x0F, video_memory, 0);
+                                        idx++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    else if (inputdata[0] == 'r' && inputdata[1] == 'e' && inputdata[2] == 'a' &&
+                             inputdata[3] == 'd')
+                    {
+                        if (inputdata[4] == '\0')
+                        {
+                            print("Error: Please provide a filename! Example: makefile hi.txt\n", 0x0F, video_memory, 0);
+                        }
+                        char *filename = &inputdata[5];
+                        readfiles(filename,video_memory);
+                    }
+                         else if (inputdata[0] == 'l' && inputdata[1] == 'i' && inputdata[2] == 's' &&
+                                    inputdata[3] == 't')
+                    {
+                       
+                        for (int i = 0; i < MAX_FILES; i++)
+                        {
+
+                            print(directory_table[i].name, 0x0F, video_memory, 0);
+                            print("\n", 0x0F, video_memory, 0);
+
+                        }
+                        
+
+                    }
+
                     else
                     {
                         print("Invalid command !\n", 0x0F, video_memory, 0);
@@ -255,104 +527,27 @@ void initInputData(char *video_memory)
     }
 }
 
-void outb(unsigned short port, unsigned char data)
-{
-    __asm__ volatile("outb %0,%1" : : "a"(data), "Nd"(port));
-}
-
-void outw(unsigned short port, unsigned short data)
-{
-    __asm__ volatile("outw %0, %1" : : "a"(data), "Nd"(port));
-}
-
-void read_disk(uint32_t lba, uint8_t *buffer)
-{
-
-    outb(0x1F2, 1); // in this port we are just sending one byte to the port 0xF2 and asking for only read one sector
-
-    // now we will send the lba adress in 8bits because the size maximum bit we can send is 8 or 1 byte so we will
-    // sift the 32 bit lba into 3 bit the total size two get one request to the port is 28 bit so (8+8+8) = 24
-    // the remaning 4 will be send on 0x1F6 port this all will be added up and then we can successfully send our
-    // 28bit lba adreess
-
-    // examples ===>   suppose we send this lba adress 0x02345f2
-    // first we will send f2 on (0x1F3)---> which we will say low
-    // second we will send 45 on (0x1F4) ----> which will say medium
-    // third we will send  23 on (0x1F5) ----> which will say high
-    // last the 0 on (0x1F6) which is neutral and will be sended on 0x1f6 port easy
-
-    // also >>, << these are right and left shift which we will use to manupulate value and send in low,medium,high bit
-
-    outb(0x1F3, (uint8_t)lba);         // low 8 bit eg: f2
-    outb(0x1F4, (uint8_t)(lba >> 8));  // medium 8 bit eg:45
-    outb(0x1F5, (uint8_t)(lba >> 16)); // high 8 bit eg: 23
-    outb(0x1F6, 0xE0 | ((lba >> 24) && 0x0F));
-
-    outb(0x1F7, 0x20); // 0x20 is read command which will be sended to disk controller
-
-    // wait for the drivers or we can say disk conroller to done work
-
-    while ((inputbyte(0x1F7) && 0x08) == 0)
-    {
-        // when ever not equal to zero loop stops and now we can perform other actions
-    }
-
-    for (int i = 0; i < 256; i++)
-    {
-        uint16_t data = 0;
-
-        // this assembly instruction is also a boiler template can be get from stack overflow
-        // it takes value from port 0x1F0 store it into data varaible which is like this
-        // in intel assembly
-        // inw dx,ax   where ax is port and dx is variable or register
-
-        __asm__ volatile("inw %%dx, %%ax" : "=a"(data) : "d"(0x1F0));
-
-        // so in first iteration we will 0*2 = 0 so buffer[0] = value from last of orignal value
-        // and then when we do ==>  0*2+1 we get 1 so buffer[1] = a 8 bit data which is in
-        // first but moved to the last by me because uint8_t choose from last bro!!
-
-        buffer[i * 2] = (uint8_t)data;
-        buffer[i * 2 + 1] = (uint8_t)(data >> 8);
-    }
-}
-
-void write_disk_sector(uint32_t lba, uint8_t *buffer)
-{
-
-    outb(0x1F2, 1);
-    outb(0x1F3, (uint8_t)lba);
-    outb(0x1F4, (uint8_t)(lba >> 8));
-    outb(0x1F5, (uint8_t)(lba >> 16));
-    outb(0x1F6, 0xE0 | ((lba >> 24) & 0x0F));
-
-    // Send the WRITE command (0x30) instead of read
-    outb(0x1F7, 0x30);
-
-    // Wait for the disk to be ready to accept data
-    while ((inputbyte(0x1F7) & 0x08) == 0)
-    {
-    }
-
-    // Push our 512 bytes from RAM into the disk
-
-    for (int i = 0; i < 256; i++)
-    {
-
-        uint16_t data = buffer[i * 2] | (buffer[i * 2 + 1] << 8);
-        // check readfuction you will notice that we are reading from ax and putting in dx
-
-        __asm__ volatile("outw %%ax, %%dx" : : "a"(data), "d"(0x1F0));
-    }
-}
-
 void kernel_main()
 {
     char *video_memory = (char *)0xB8000;
 
     cleanscreen(video_memory, 0x00);
     print("WELCOME TO SKOS \n", 0x0F, video_memory, 0);
-    print("> ", 0x0F, video_memory, 0);
 
+    // 1. Load directory from disk right away on bootup
+    load_directory();
+
+    // 2. If the disk is uninitialized (garbage values), format it once cleanly.
+    if (directory_table[0].is_used != 0 && directory_table[0].is_used != 1)
+    {
+        print("First boot or corrupted disk! Formatting file allocation table...\n", 0x0F, video_memory, 0);
+        format_disk_directory();
+    }
+    else
+    {
+        print("Disk file system structure verified successfully.\n", 0x0F, video_memory, 0);
+    }
+
+    print("> ", 0x0F, video_memory, 0);
     initInputData(video_memory);
 }
